01d2129a7152d749b412c59fec62b55382eb978e
Wedaad <wedaadh2@gmail.com>
2021-02-10 18:35:25 +0000

    lab3 added case5, case6 and case7

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -62,7 +62,7 @@ public class Loops extends PApplet {
                 }
                 break;
             case 2:
-            {
+    
                 int numRects = (int)(mouseX / 10.0f);
                 float w = width / (float) numRects;
                 float cgap = 255 / (float) numRects;
@@ -72,19 +72,81 @@ public class Loops extends PApplet {
                     rect(i * w, 0, w, height);
                 }
                 break;
-            }
+    
             case 3:
-            {
+
                 int numCircles = (int)(mouseX / 10.0f);
-                float w = width / (float) numCircles;
-                float cgap = 255 / (float) numCircles;
+                float cw = width / (float) numCircles;
+                float cirgap = 255 / (float) numCircles;
                 for(int i = 0 ; i < numCircles ; i ++)
                 {
-                    fill(cgap * i, 255, 255);
-                    ellipse(w / 2 + (i * w), cy, w, w);
+                    fill(cirgap * i, 255, 255);
+                    ellipse(cw / 2 + (i * cw), cy, cw, cw);
                 }
-            }
             break;
-        }
+
+            case 4:
+                
+                stroke(50, 255, 255);
+                int numLines = 5;
+                float theta = TWO_PI / (float) numLines; //gives angle between lines
+                float radius = 100;
+
+                for(int i = 0; i < numLines; i++) {
+
+                    float angle = theta * i;
+                    float x = sin(angle) * radius;
+                    float y = cos(angle) * radius;
+                    line(cx, cy, cx + x, cy + y);
+                }
+            break;
+
+            case 5: 
+                int numSquares = 10;
+                float sw = width / (float) numSquares;
+                float squaregap = 255 / (float) numSquares;
+
+
+                for(int i = 0; i < numSquares; i++) {
+
+                    fill(squaregap * i, 255, 255);
+                    square((i * sw), (i * sw), sw);
+                        
+                }
+            break;
+
+            case 6: 
+
+                noStroke();
+                int nCircles = 10; 
+                float cirColour = 255 / (float) nCircles;
+                float cirGap = 500 / (float) nCircles;
+
+                for(int i = 0; i < nCircles; i++){
+
+                    fill(cirColour * i, 255, 255);
+                    ellipse(cx, cy, (width - (cirGap * i)), (height - (cirGap * i)));
+                    
+                }
+            break;
+
+            case 7:
+                int circles = 10;
+                float circlew = width / (float) circles;
+                float circleGap = 255 / (float) circles;
+                
+                for(int i = 0; i < circles; i++) {
+
+                    for(int j = 0; j < circles; j++){
+
+                        fill(circleGap * i, 255, 255);
+                        ellipse(circlew / 2 + (i * circlew), circlew / 2 + (j * circlew), circlew, circlew);
+
+                    }
+                }
+            break;
+    
+                
+        }   
     }
 }
