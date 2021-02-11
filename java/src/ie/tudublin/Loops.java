8b88aa7ff1ac9de2e05096dd4d3d735515ae8d84
Wedaad <wedaadh2@gmail.com>
2021-02-11 10:02:12 +0000

    add more cases of procedural drawings

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -26,12 +26,14 @@ public class Loops extends PApplet {
         colorMode(HSB);
     }
 
+    float offset = 0;
+
     public void draw() {
         background(0);
         noStroke();
         switch (mode)
         {
-            case 0:
+            case 0: {
                 fill(50, 255, 255);                    
                 if (mouseX < cx)
                 {
@@ -42,7 +44,11 @@ public class Loops extends PApplet {
                     rect(cx, 0, cx, height);
                 }
                 break;
-            case 1:
+                
+            }//ends case 0
+
+            case 1: {
+
                 fill(50, 255, 255);                                    
                 if (mouseX < cx && mouseY < cy)
                 {
@@ -61,7 +67,9 @@ public class Loops extends PApplet {
                     rect(cx, cy, cx, cy);
                 }
                 break;
-            case 2:
+            }//ends case 1
+            
+            case 2: {
     
                 int numRects = (int)(mouseX / 10.0f);
                 float w = width / (float) numRects;
@@ -72,8 +80,10 @@ public class Loops extends PApplet {
                     rect(i * w, 0, w, height);
                 }
                 break;
+
+            }//ends case 2
     
-            case 3:
+            case 3: {
 
                 int numCircles = (int)(mouseX / 10.0f);
                 float cw = width / (float) numCircles;
@@ -83,9 +93,10 @@ public class Loops extends PApplet {
                     fill(cirgap * i, 255, 255);
                     ellipse(cw / 2 + (i * cw), cy, cw, cw);
                 }
-            break;
+                break;
+            }//ends case 3
 
-            case 4:
+            case 4: {
                 
                 stroke(50, 255, 255);
                 int numLines = 5;
@@ -99,9 +110,10 @@ public class Loops extends PApplet {
                     float y = cos(angle) * radius;
                     line(cx, cy, cx + x, cy + y);
                 }
-            break;
+                break;
+            }//ends case 4
 
-            case 5: 
+            case 5: {
                 int numSquares = 10;
                 float sw = width / (float) numSquares;
                 float squaregap = 255 / (float) numSquares;
@@ -113,9 +125,10 @@ public class Loops extends PApplet {
                     square((i * sw), (i * sw), sw);
                         
                 }
-            break;
+                break;
+            }//ends case 5
 
-            case 6: 
+            case 6: {
 
                 noStroke();
                 int nCircles = 10; 
@@ -128,25 +141,67 @@ public class Loops extends PApplet {
                     ellipse(cx, cy, (width - (cirGap * i)), (height - (cirGap * i)));
                     
                 }
-            break;
+                break;
+            }//ends case 6
 
-            case 7:
+            case 7: {
+
+                offset += (mouseX / 100);
                 int circles = 10;
                 float circlew = width / (float) circles;
-                float circleGap = 255 / (float) circles;
+                float circleGap = 255 / (circles + circles);
                 
                 for(int i = 0; i < circles; i++) {
 
                     for(int j = 0; j < circles; j++){
-
-                        fill(circleGap * i, 255, 255);
+                        
+                        float c = ((circleGap * (i + j) + offset) % 255); //giving the colour a range between 0 - 255 using modulus operator
+                        fill(c, 255, 255);
                         ellipse(circlew / 2 + (i * circlew), circlew / 2 + (j * circlew), circlew, circlew);
 
                     }
                 }
-            break;
-    
+                break;
+            }//ends case 7
+
+            case 8: {
                 
+                float ww = 200;
+                float h = 50;
+                rectMode(CENTER);
+
+                if(mouseX > cx - (ww/2) && mouseX < cx + (ww/2) && mouseY > cy - (h / 2) &&  mouseY < cy + (h / 2)) {
+
+                    fill(50, 255, 255);
+
+                }
+                else {
+
+
+                    fill(200, 255, 255);
+                }
+                
+                rect(cx, cy, ww, h);
+                break;  
+            }//ends case 8
+            case 9: {
+
+                int sides = (mouseX / 50);
+                float thetas = TWO_PI / (float) sides;
+                float rad = 200;
+                stroke(255);
+
+                for(int i = 1; i <= sides; i++){
+
+                    float x1 = sin(thetas * (i - 1)) * rad;
+                    float y1 = cos(thetas * (i - 1)) * rad;
+                    float x2 = sin(thetas * i) * rad;
+                    float y2 = cos(thetas * i) * rad;
+                    line(cx + x1, cy+ y1, cx + x2, cy + y2);
+                } 
+                break;
+            }//ends case 9
+
         }   
     }
 }
